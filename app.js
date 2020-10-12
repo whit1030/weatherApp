36c27fde3ff4abbacc2a9963bc1e238cca546288
whit1030 <whit1030@algonquinlive.com>
2020-10-12 18:42:34 -0400

    fix api link

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-const api = fetch('http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=df7374ae099bb5c3225c10b111a545af');
+const api = fetch('https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=df7374ae099bb5c3225c10b111a545af');
